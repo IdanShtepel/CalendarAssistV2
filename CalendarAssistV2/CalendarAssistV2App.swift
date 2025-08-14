2041e5522069483d06fc355fcb3f40c2b0e2b486
Idan <idan.shtepel2@gmail.com>
2025-08-13 19:19:55 -0700

    Initial Commit

---
+++ CalendarAssistV2/CalendarAssistV2App.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAssistV2App.swift
+//  CalendarAssistV2
+//
+//  Created by Idan Shtepel on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAssistV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarAssistV2Tests/CalendarAssistV2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarAssistV2Tests.swift
+//  CalendarAssistV2Tests
+//
+//  Created by Idan Shtepel on 8/13/25.
+//
+
+import Testing
+
+struct CalendarAssistV2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
